c68aa738956f19dee8a038866b2d476eb371b73c
Ruben Vermeersch <ruben@rocketeer.be>
2013-11-14 15:09:36 +0100

    Use connect-body-rewrite.

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 'use strict';
 
+var rewrite = require('connect-body-rewrite');
+
 module.exports = function (options) {
     if (!options) {
         options = {};
     }
     var host = options.host || "http://localhost:35729";
-    var script = "<script src=\"" + host + "/livereload.js?snipver=1\"></script>";
+    var script = "<script src=\"" + host + "/livereload.js?snipver=1\"></script>\n";
 
-    return function (req, res, next) {
-        var write = res.write;
-
-        res.write = function (chunk, encoding, callback) {
-            var length = res.getHeader('content-length');
-            var header = res.getHeader('content-type');
-
-            if (header && header.match(/text\/html/) && chunk) {
-                chunk = chunk.toString();
-                var origLength = chunk.length;
-                chunk = chunk.replace(/(<\/body>)/, script + "$1");
-                var newLength = chunk.length;
-
-                res.setHeader('content-length', length - (origLength - newLength));
-            }
-            write.call(res, chunk, encoding, callback);
-        };
-        next();
-    };
+    return rewrite({
+        accept: function (res) {
+            return res.getHeader('content-type').match(/text\/html/);
+        },
+        rewrite: function (body) {
+            return body.replace(/(<\/body>)/, script + "$1");
+        }
+    });
 };
